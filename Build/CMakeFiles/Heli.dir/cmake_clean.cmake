1220aeb238287a92ce7db8f9f0ef38a1d1524689
sc21sv <sauliusvince@gmail.com>
2023-06-05 17:57:24 +0100

    Cmake

---
+++ Build/CMakeFiles/Heli.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heli.dir/Heli/Core/ECS/Entity.cpp.o"
+  "CMakeFiles/Heli.dir/Heli/Core/ECS/Entity.cpp.o.d"
+  "CMakeFiles/Heli.dir/Heli/Core/ECS/SystemManager.cpp.o"
+  "CMakeFiles/Heli.dir/Heli/Core/ECS/SystemManager.cpp.o.d"
+  "CMakeFiles/Heli.dir/Heli/Core/Logging/Debug.cpp.o"
+  "CMakeFiles/Heli.dir/Heli/Core/Logging/Debug.cpp.o.d"
+  "CMakeFiles/Heli.dir/Heli/Core/Memory/MemoryManager.cpp.o"
+  "CMakeFiles/Heli.dir/Heli/Core/Memory/MemoryManager.cpp.o.d"
+  "CMakeFiles/Heli.dir/main.cpp.o"
+  "CMakeFiles/Heli.dir/main.cpp.o.d"
+  "Heli"
+  "Heli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Heli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
