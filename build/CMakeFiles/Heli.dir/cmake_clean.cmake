1bdcdbeaa816b9ecb85dbe2dbc5511e75b489191
Saulius Vincevičius <sauliusvince@gmail.com>
2024-04-16 20:59:24 +0200

    OpenGL

---
+++ build/CMakeFiles/Heli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heli.dir/main.cpp.o"
+  "CMakeFiles/Heli.dir/main.cpp.o.d"
+  "Heli"
+  "Heli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Heli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
